6682565016f1da5d785e5f1dded7766fcb58a21c
Jyrone Parker <jyrone.parker@gmail.com>
2015-11-09 14:42:08 -0800

    Added dialogs and beeps yay!

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('parker-pay', ['ionic'])
+angular.module('parker-pay', ['ionic','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,9 +17,13 @@ angular.module('parker-pay', ['ionic'])
     }
   });
 })
-.controller('HomeCtrl', function($scope,$http){
+.controller('HomeCtrl', function($scope,$http,$cordovaSpinnerDialog,$cordovaDialogs){
+
+
+
   $scope.charge = {};
   $scope.createToken = function(){
+    $cordovaSpinnerDialog.show("Processing Payment","Please wait....", true);
     Stripe.setPublishableKey('pk_test_QgdJSyRF8v7QJVM4mudy99XN');
     Stripe.card.createToken({
   number: $scope.charge.number,
@@ -27,11 +31,18 @@ angular.module('parker-pay', ['ionic'])
   exp_month: $scope.charge.exp_month,
   exp_year: $scope.charge.exp_year
 }, $scope.stripeResponseHandler);
+
 };
 $scope.stripeResponseHandler = function(status, response){
   if (response.error) {
   // Show the errors on the form
-  alert('There was an error');
+  $cordovaSpinnerDialog.hide();
+  $cordovaDialogs.alert('There was an error', 'Alert', 'OK')
+    .then(function() {
+      // callback success
+    });
+
+
 } else {
   // response contains id and card, which contains additional card details
   var token = response.id;
@@ -39,12 +50,27 @@ $scope.stripeResponseHandler = function(status, response){
   // Insert the token into the form so it gets submitted to the server
   var data = {token:token,amount:$scope.charge.amount,description:$scope.charge.description}
   // and submit
+
   $http.post('http://payment.jyroneparker.com/charge',data).success(function(dta){
     console.log(dta);
-    alert('Payment was a success.');
+    $cordovaSpinnerDialog.hide();
+    // beep 1 time
+    $cordovaDialogs.beep(1);
+    $cordovaDialogs.alert('Payment was a success.', 'Alert', 'OK')
+      .then(function() {
+        // callback success
+      });
+
+
   }).error(function(dta){
     console.log(dta);
-    alert('There was an error.')
+    $cordovaSpinnerDialog.hide();
+    alert('There was an error.');
+    $cordovaDialogs.alert('There was an error', 'Alert', 'OK')
+      .then(function() {
+        // callback success
+      });
+    //$cordovaSpinnerDialog.hide();
   });
 }
 }
